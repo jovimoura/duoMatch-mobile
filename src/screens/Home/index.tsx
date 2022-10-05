0fb0988bfb2aab03bc17192b388a7b2616b97329
jovimoura <joaovictors.mouraa@gmail.com>
2022-10-05 14:56:54 -0300

    backend connected

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,29 +1,39 @@
-import React from 'react'
-import { FlatList, Image, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { FlatList, Image, ScrollView } from 'react-native'
 
 import { styles } from './styles'
 
 import logoImg from '../../assets/logo-nlw-esports.png'
 import { Heading } from '../../components/Heading'
 import { GAMES } from '../../utils/games'
-import { GameCard } from '../../components/GameCard'
+import { GameCard, GameCardProps } from '../../components/GameCard'
 
 export function Home() {
+  const [data, setData] = useState<GameCardProps[]>([])
+
+  useEffect(() => {
+    fetch('http://192.168.0.19:3333/games')
+      .then(res => res.json())
+      .then(data => setData(data))
+  }, [])
   return (
-    <View style={styles.container}>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={{ alignItems: 'center', paddingBottom: 64 }}
+    >
       <Image source={logoImg} style={styles.logo} />
       <Heading
         title="Encontre seu duo!"
         subtitle="Selecione o game que deseja jogar..."
       />
       <FlatList
-        data={GAMES}
+        data={data}
         keyExtractor={item => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         horizontal
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.contentList}
       />
-    </View>
+    </ScrollView>
   )
 }
